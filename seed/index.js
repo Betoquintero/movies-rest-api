658f3c47b22501a9bf8a0091a262a4b94d1c7dd0
Alberto <aquintero88@gmail.com>
2022-08-28 11:06:21 +0200

    done

--- seed/index.js
+++ seed/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Movie = require ('../models/Movie');
 
 // Add the model and array you want to seed
+const movies =[
+{
+  "title": "Home Alone",
+  "year": 1990,
+  "director": "Chris Colombus",
+  "duration": 103,
+  "synopsis": "A kid is left alone at home while his family travels to Paris for Christmas. He then has to face two burglars with much creativity.",
+  "image": "https://m.media-amazon.com/images/I/81EWrTadiJL._AC_SY679_.jpg"
+}
+]
+
 
 mongoose.connect(process.env.MONGO_URL)
   .then(x => console.log(`Connected to ${x.connection.name}`))
   .then(() => {
-    return
+    return Movie.create(movies)
   })
   .then(() => {
     console.log('Seed done 🌱');
